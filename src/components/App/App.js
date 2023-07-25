fa29a9e9b663a49f6af6ad6d75c00708b37ddb50
Tati Pro 3 <prokhorova.tati@yandex.ru>
2023-07-25 05:49:25 +0300

    first touch

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+// модули
+// import { useEffect, useState } from "react";
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// компоненты
+
+
+function App() {
+  // функции
+
+  //разметка
+  return (
+    <div className="page">
+    </div>
+  );
+}
+
+export default App;
